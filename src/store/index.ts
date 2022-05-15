49e6461269f226770c6ebbef48f7e9ad1b0d5f3a
Anthony Clausing <anthonyclausing@gmail.com>
2022-05-14 22:59:09 -0400

    add types to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,28 @@
-import { createStore } from "vuex";
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+export interface State {
+  user: string;
+  loading: boolean;
+}
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export const store = createStore<State>({
+  state(): State {
+    return {
+      user: "",
+      loading: false,
+    };
+  },
   actions: {},
-  modules: {},
+  mutations: {
+    SET_LOADING(state, bool) {
+      state.loading = bool;
+    },
+    SET_USER(state, str) {
+      state.user = str;
+    },
+  }
 });
+export default store;
