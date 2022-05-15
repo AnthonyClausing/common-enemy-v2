cc7df0356e67be45651661d563ec5c427e722f3f
Anthony Clausing <anthonyclausing@gmail.com>
2022-05-14 23:18:51 -0400

    Basic logic complete  - populate state with necesary data  - around 80% complete

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -10,20 +10,112 @@
           </li> -->
       </ul>
     </div>
-    <!-- <top-enemies :championCount="getAnswer"></top-enemies> -->
+    <!-- <top-enemies></top-enemies> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import axios from "axios";
+import _ from "lodash";
+// import TopEnemies from "./TopEnemies.vue";
+const ROOT_URL = "http://localhost:3000/api";
 export default defineComponent({
   name: "HomeView",
+  components: {
+    // TopEnemies,
+  },
   data() {
     return {
       summonerName: "SaltFreeRobot",
       summonerPuuid: "",
       result: {},
     };
+  },
+  computed: {
+    // isLoading(){
+    //   return state.loading
+    // },
+  },
+  mounted() {
+    // TODO Turn below axios calls into state actions
+    axios
+      .get(`${ROOT_URL}/summoner?name=${this.summonerName}`)
+      .then((res) => {
+        this.summonerPuuid = res.data.puuid;
+        return axios.get(`${ROOT_URL}/matches?puuid=${res.data.puuid}`);
+      })
+      .then((res) => this.getMatchListInfo(res.data.matchIds))
+      .then((newMatchList) => {
+        this.result = this.getChampCounts(newMatchList);
+      })
+      .catch((err) => console.error(err));
+  },
+  methods: {
+    getMatchListInfo: async function (matchIds: string[]) {
+      let matches: any = [];
+      for (let matchId of matchIds) {
+        let { data } = await axios.get(`${ROOT_URL}/match?matchId=${matchId}`);
+        matches.push(data);
+      }
+      return matches;
+    },
+    //can add another parameter like a bool for checking for bad allies putting it in the if block
+    getChampCounts: function (matches: any) {
+      let championCounter: any = {
+        bonnie: {},
+        brutus: {},
+        nemisis: {},
+      };
+      matches.forEach((match: any) => {
+        let userMatchInfo = _.find(match.participants, [
+          "puuid",
+          this.summonerPuuid,
+        ]);
+        if (!userMatchInfo.win) {
+          match.participants.forEach((player: any) => {
+            if (player.puuid === userMatchInfo.puuid) return;
+            if (player.teamId !== userMatchInfo.teamId) {
+              if (championCounter.nemisis[player.championId]) {
+                championCounter.nemisis[player.championId].count++;
+              } else {
+                championCounter.nemisis[player.championId] = {
+                  count: 1,
+                  name: player.championName,
+                  image: `${player.championName}.png`,
+                };
+              }
+            } else {
+              if (championCounter.brutus[player.championId]) {
+                championCounter.brutus[player.championId].count++;
+              } else {
+                championCounter.brutus[player.championId] = {
+                  count: 1,
+                  name: player.championName,
+                  image: `${player.championName}.png`,
+                };
+              }
+            }
+          });
+        } else {
+          match.participants.forEach((player: any) => {
+            if (player.puuid === userMatchInfo.puuid) return;
+            if (player.teamId === userMatchInfo.teamId) {
+              if (championCounter.bonnie[player.championId]) {
+                championCounter.bonnie[player.championId].count++;
+              } else {
+                championCounter.bonnie[player.championId] = {
+                  count: 1,
+                  name: player.championName,
+                  image: `${player.championName}.png`,
+                };
+              }
+            }
+          });
+        }
+      });
+      return championCounter;
+    },
   },
 });
 </script>
---
+++ src/components/TopEnemies.vue
@@ -0,0 +1,10 @@
+<template>
+<div>Top Enemies</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HomeView",
+})
+</script>
